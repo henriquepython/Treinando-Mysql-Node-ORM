f2c4b30cb995c2ad70277c661004929b54b207d0
JOÃO HENRIQUE BATISTA CERQUEIRA <joaohbp59@gmail.com>
2021-10-11 13:38:10 -0300

    CRUD FINALIZADO

--- src/controller/CadastroContoller.ts
+++ src/controller/CadastroContoller.ts
@@ -5,19 +5,35 @@ import knex from '../database/connection';
 export default {
     async create(req: Request, res: Response) {
         const {nome, cpf} = req.body;
-        const id = 3;
-        const data = {id, nome, cpf};
+        const data = {cpf, nome};
+        await knex('atb_cadastro').insert(data);
         return res.status(201).json({ data:data});
     },
     async list(req: Request, res: Response) {
         var result = await knex('atb_cadastro').orderBy('nome');
         return res.status(200).json({ data:result });
     },
+    async find(req: Request, res: Response) {
+        const { id } = req.params;
+        const user = await knex('atb_cadastro').where({ id });
+        return res.status(200).json({ data:user });
+    },
     async update(req: Request, res: Response) {
+        const { id } = req.params;
         const { nome, cpf } = req.body;
-        const dataAlteracao = '30/05/2021 16:00'
-        const cadastro = {nome, cpf, dataAlteracao};
-        cadastro.nome = "JOSE DA SILVA LIMA";
+        const data = {cpf, nome};
+
+        await knex('atb_cadastro').update(data).where({ id });
+
+        const cadastro = await knex('atb_cadastro').where({ id });
+
         return res.status(200).json({ data:cadastro});
+    },
+    async delete(req: Request, res: Response) {
+        const { id } = req.params;
+        
+        await knex('atb_cadastro').del().where({ id });
+
+        return res.status(200).json({ message: 'Registro excluido com sucesso!'});
     }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -6,7 +6,9 @@ import CadastroContoller from "./controller/CadastroContoller";
 const routes = Router();
 
 routes.post('/cadastro', CadastroContoller.create);
-routes.put('/cadastro', CadastroContoller.update);
+routes.put('/cadastro/:id', CadastroContoller.update);
 routes.get('/cadastro', CadastroContoller.list);
+routes.get('/cadastro/:id', CadastroContoller.find);
+routes.delete('/cadastro/:id', CadastroContoller.delete);
 
 export default routes; 
