5f5a60e1d69afdc0e7f487285476a345b1fe4115
JOÃO HENRIQUE BATISTA CERQUEIRA <joaohbp59@gmail.com>
2021-10-12 09:48:07 -0300

    UPDATE ORM

--- src/controller/CadastroContoller.ts
+++ src/controller/CadastroContoller.ts
@@ -1,39 +1,37 @@
 import { Request, Response } from 'express';
 
-import knex from '../database/connection';
+
 
 export default {
     async create(req: Request, res: Response) {
         const {nome, cpf} = req.body;
         const data = {cpf, nome};
-        await knex('atb_cadastro').insert(data);
+        
         return res.status(201).json({ data:data});
     },
     async list(req: Request, res: Response) {
-        var result = await knex('atb_cadastro').orderBy('nome');
+        const result = null;
         return res.status(200).json({ data:result });
     },
     async find(req: Request, res: Response) {
         const { id } = req.params;
-        const user = await knex('atb_cadastro').where({ id });
-        return res.status(200).json({ data:user });
+        const cadastro = null;
+        return res.status(200).json({ data:cadastro });
     },
     async update(req: Request, res: Response) {
         const { id } = req.params;
         const { nome, cpf } = req.body;
         const data = {cpf, nome};
 
-        await knex('atb_cadastro').update(data).where({ id });
+        
 
-        const cadastro = await knex('atb_cadastro').where({ id });
+        const cadastro = null;
 
         return res.status(200).json({ data:cadastro});
     },
     async delete(req: Request, res: Response) {
         const { id } = req.params;
         
-        await knex('atb_cadastro').del().where({ id });
-
         return res.status(200).json({ message: 'Registro excluido com sucesso!'});
     }
 }
--- src/database/connection.ts
+++
@@ -1,5 +0,0 @@
-//ação de disponibilizar o banco
-import knex from "knex";
-import knexFile from "../../knexfile";
-export default knex(knexFile['development']);
-console.log("Conexão realizada com sucesso !");
