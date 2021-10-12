1a26b1ee825b84a5364c77eb5b68b0387d965f03
JOÃO HENRIQUE BATISTA CERQUEIRA <joaohbp59@gmail.com>
2021-10-12 10:47:09 -0300

    UPDATE CRUD

--- src/controller/CadastroContoller.ts
+++ src/controller/CadastroContoller.ts
@@ -1,21 +1,31 @@
 import { Request, Response } from 'express';
+import {getCustomRepository} from 'typeorm';
+
+import {CadastroRepository} from "../repositories/CadastroRepository";
 
 
 
 export default {
     async create(req: Request, res: Response) {
         const {nome, cpf} = req.body;
-        const data = {cpf, nome};
+        var data = {cpf, nome};
         
+        const repository = getCustomRepository(CadastroRepository);
+        data = await repository.save(data); 
+
         return res.status(201).json({ data:data});
     },
     async list(req: Request, res: Response) {
-        const result = null;
-        return res.status(200).json({ data:result });
+        const repository = getCustomRepository(CadastroRepository);
+        const data = await repository.find();
+        
+        return res.status(200).json({ data:data });
     },
     async find(req: Request, res: Response) {
         const { id } = req.params;
-        const cadastro = null;
+        const repository = getCustomRepository(CadastroRepository);
+        const cadastro = await repository.findOne(id);
+        
         return res.status(200).json({ data:cadastro });
     },
     async update(req: Request, res: Response) {
@@ -23,14 +33,16 @@ export default {
         const { nome, cpf } = req.body;
         const data = {cpf, nome};
 
-        
+        const repository = getCustomRepository(CadastroRepository);
+        await repository.update( id, data);
 
-        const cadastro = null;
-
-        return res.status(200).json({ data:cadastro});
+        return res.status(200).json({ data:"cadastro atualizado com sucesso!"});
     },
     async delete(req: Request, res: Response) {
         const { id } = req.params;
+        const repository = getCustomRepository(CadastroRepository);
+        await repository.delete( id);
+        
         
         return res.status(200).json({ message: 'Registro excluido com sucesso!'});
     }
---
+++ src/entities/Cadastro.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+
+@Entity("tab_cadastro")
+export class Cadastro{
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    cpf: string;
+
+    @Column()
+    nome: string;
+}
